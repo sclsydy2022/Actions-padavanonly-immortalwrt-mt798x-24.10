0ceaf07d247d7479a367f752dc9ee698a3bfa203
sclsydy2022 <98293456+sclsydy2022@users.noreply.github.com>
2025-04-14 04:26:20 +0800

    Update padavanonly-part2.sh

--- padavanonly-part2.sh
+++ padavanonly-part2.sh
@@ -9,7 +9,7 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# 解决冲突
+# 解决冲突（适用于kenzok8插件源码）
 # rm -rf feeds/luci/applications/luci-app-mosdns
 # rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 # rm -rf feeds/packages/utils/v2dat
