92b2e7dd368cf4bee38c62ad478a4327f9c75bc3
sclsydy2022 <98293456+sclsydy2022@users.noreply.github.com>
2025-08-04 15:26:48 +0800

    Update padavanonly-part1.sh

--- padavanonly-part1.sh
+++ padavanonly-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
